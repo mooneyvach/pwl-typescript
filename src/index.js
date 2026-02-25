15be960cc1c0816459621a7095e25a339ee31f95
mooneyvach <h1101241027@student.untan.ac.id>
2026-02-25 05:01:46 +0000

    update index

---
+++ src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(nama, umur) {
+    return `Selamat datang ${nama} di PWL, sekarang kamu berumur ${umur} tahun.`;
+}
+const mahasiswa = "Muni";
+const umur = 15;
+console.log(hello(mahasiswa, umur));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+function hello(nama: string, umur: number): string {
+    return `Selamat datang ${nama} di PWL, sekarang kamu berumur ${umur} tahun.`;
+}
+
+const mahasiswa = "Muni";
+const umur = 15;
+console.log(hello(mahasiswa, umur));
